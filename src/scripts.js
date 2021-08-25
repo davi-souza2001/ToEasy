a7af1356fef55dd7684edc94f4721b614caca557
davi-souza2001 <davisouza2001dv@gmail.com>
2021-08-25 19:35:36 -0300

    bug and  reorganization

--- src/scripts.js
+++
@@ -1,9 +0,0 @@
-function test (){
-    const items = document.getElementsByClassName("apps")
-    items.innerHTML = `
-            <h4>Lol</h4>
-                <h4>Vava</h4>
-            <h4>Estudos</h4>
-    `
-    console.log("funcionando")
-}
