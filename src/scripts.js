b50534f01a8d04f295b34f9633cc02b202c344f6
davi-souza2001 <davisouza2001dv@gmail.com>
2021-08-25 17:51:52 -0300

    bug in html

---
+++ src/scripts.js
@@ -0,0 +1,9 @@
+function test (){
+    const items = document.getElementsByClassName("apps")
+    items.innerHTML = `
+            <h4>Lol</h4>
+                <h4>Vava</h4>
+            <h4>Estudos</h4>
+    `
+    console.log("funcionando")
+}
