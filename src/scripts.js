2b7fb691359ff1b14abc5498769f3e66c753ce1f
davi-souza2001 <davisouza2001dv@gmail.com>
2021-08-27 22:47:36 -0300

    update project

--- src/scripts.js
+++
@@ -1,9 +0,0 @@
-function send() {
-    let apps = document.getElementById("apps");
-    let title = document.getElementById("txtTitle").value;
-    let div = document.querySelector("h4");
-    console.log(apps);
-    apps.style.display = "flex";
-     
-    div.innerText = `${title}`;
-}
